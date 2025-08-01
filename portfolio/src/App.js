e8e7ec7a40d44054f5ae88beaba796d4a8e6acb0
Xuban <59646791+EHxuban11@users.noreply.github.com>
2025-08-01 10:25:45 +0200

    removed every page except about

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,54 +1,13 @@
-import './App.css';
 import React from 'react';
-import { HashRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import { AppBar, Toolbar, Typography, Box, CssBaseline, Button, Container } from '@mui/material';
+import { CssBaseline } from '@mui/material';
 import About from './Pages/About';
-import PersonalProjects from './Pages/PersonalProjects';
-import HackathonsAndPrograms from './Pages/HackathonsAndPrograms';
-import Tools from './Pages/Tools';
 
 function App() {
   return (
-    <Router>
+    <>
       <CssBaseline />
-      <div className="App">
-        <AppBar position="fixed" sx={{ backgroundColor: '#777', boxShadow: 'none' }}>
-          <Container maxWidth="md">
-            <Toolbar sx={{ justifyContent: 'space-between' }}>
-              <Typography variant="h6" component="div">
-                Xuban C.
-              </Typography>
-              <Box sx={{ display: 'flex' }}>
-                {/*
-                <Button color="inherit" component={Link} to="/about">
-                  About
-                </Button>
-                <Button color="inherit" component={Link} to="/personal-projects">
-                  Personal Projects
-                </Button>
-                */}
-                <Button color="inherit" component={Link} to="/hackathons-and-programs">
-                  Hackathons
-                </Button>
-                {/* <Button color="inherit" component={Link} to="/tools">Tools</Button> */}
-              </Box>
-            </Toolbar>
-          </Container>
-        </AppBar>
-
-        <Box sx={{ pt: 8 }}>
-          <Routes>
-            {/*
-            <Route exact path="/" element={<About />} />
-            <Route path="/personal-projects" element={<PersonalProjects />} />
-            <Route path="/about" element={<About />} />
-            */}
-            <Route path="/hackathons-and-programs" element={<HackathonsAndPrograms />} />
-            <Route path="/tools" element={<Tools />} />
-          </Routes>
-        </Box>
-      </div>
-    </Router>
+      <About />
+    </>
   );
 }
 
